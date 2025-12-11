5355ce7b97146f77e3541d63ed0d94410ca250d0
binsu <tnqkrdl7@naver.com>
2025-12-11 13:28:36 +0900

    fix: Vercel CORS 허용

--- backend/src/main/java/com/inveskit/backend/config/WebConfig.java
+++ backend/src/main/java/com/inveskit/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")  // React 주소
+                .allowedOrigins("http://localhost:3000", "https://aider-eight.vercel.app")  // React 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
